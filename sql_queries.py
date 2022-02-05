81547ffd17d51b1cbc999f0b908381fec52cd890
Quinten Goens <qgoens@gmail.com>
2022-02-05 21:17:12 +0000

    Revision 2

--- sql_queries.py
+++ sql_queries.py
@@ -68,14 +68,22 @@ weekday integer NOT NULL
 
 songplay_table_insert = ("""
 INSERT INTO
-   songplays ( start_time, user_id, level, song_id, artist_id, session_id, location, user_agent ) 
+   songplays ( 
+   start_time, 
+   user_id, 
+   level, 
+   song_id, 
+   artist_id, 
+   session_id, 
+   location, 
+   user_agent ) 
 VALUES(%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING""")
 
 user_table_insert = ("""
 INSERT INTO
    users ( user_id, first_name, lastname, gender, level ) 
 VALUES
-   (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
+   (%s, %s, %s, %s, %s) ON CONFLICT(user_id) DO UPDATE SET level = excluded.level
 """)
 
 song_table_insert = ("""
